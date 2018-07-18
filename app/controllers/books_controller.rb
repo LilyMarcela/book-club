c150b8d48e792d7d2476742abed1c5ddbeecfd98
Mayra Rodriguez <mers85@gmail.com>
2018-07-18 19:30:47 +0200

    error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,21 +9,24 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
     @book = Book.new
+    @background_pic = "sunsetpic"
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
+
+    upload
     @book = Book.new(book_params)
     @book.owner_id = current_user.id
-
     @book.save
-
     redirect_to "/books/#{@book.id}"
   end
 
@@ -37,14 +40,14 @@ class BooksController < ApplicationController
     redirect_to "/books"
   end
 
-    def search
+  def search
     if params[:search]
       #the commented line search is a sql query, optional when elastic search has not being installed yet
-      #@books = Book.where("title LIKE ? OR author LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      @books = Book.where("title LIKE ? OR author LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
 
       # searchkick parameters, it needs to have the gem installed
       # Additionally, it relies on elastic search which uses java
-      @books = Book.search(params[:search])
+      # @books = Book.search(params[:search])
     else
       @books = Book.all
     end
@@ -69,14 +72,20 @@ class BooksController < ApplicationController
     @admin = user_signed_in? && current_user.admin
   end
 
-
   def destroy_check
+    @book = Book.find(params[:id])
     unless @admin || current_user.id == @book.owner_id
       redirect_to "/"
     end
-
   end
 
-
+  def upload
+    s3 = Aws::S3::Resource.new
+    bucket = s3.bucket("capetownpicture")
+    file = params[:book][:url_file]
+    key = current_user.id.to_s + file.original_filename
+    obj = bucket.object(key)
+    obj.upload_file(file.path)
+  end
 
 end
