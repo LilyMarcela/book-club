c91b9118c06cef765ba54f20cd6051b1dda79067
lein <lein.davir@gmail.com>
2018-07-23 11:39:14 -0500

    optimizing the pagination only when needed

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2018_07_19_184528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "owner_id"
-    t.integer "category_id"
     t.string "bookpdf"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
