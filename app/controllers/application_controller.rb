ae74010c614cd002e9af6c4fd6d881a97e6b5038
1160275520 <wangy3@carleton.edu>
2018-07-23 09:58:46 -0500

    style change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+   before_action :set_background
    def set_background
     if params[:controller] == "devise/sessions" || params[:controller] == "devise/registrations" || params[:controller] == "devise/passwords"
       @background_pic = "sunsetpic"
